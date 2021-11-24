206e5beb5aea571836b30f0cb5ea2b91435bd606
tomas <tcerdeira@itba.edu.ar>
2021-11-24 17:59:52 -0300

    primera implementacion de script bash para compilar el lenguaje

---
+++ grammar_compiler.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 1. .out compilado despues de YACC y LEX (parser)
+# 2. archivo en nuestro lenguaje pasado como parametro
+# 3. archivo salida en C tras su transofrmacion
+
+#    1.       2.    3.
+./main.out < $1 > aux.c 
+
+# 4. nombre de ejecutable final pasado como parametro
+
+#               4.
+gcc -w aux.c -o $2
+
+rm aux.c
