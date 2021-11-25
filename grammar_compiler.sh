20f89cdbf96c501c21c4cbb0ba17516ee4442af1
tomas <tcerdeira@itba.edu.ar>
2021-11-25 09:57:11 -0300

    update .sh

--- grammar_compiler.sh
+++ grammar_compiler.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# 1. .out compilado despues de YACC y LEX (parser)
-# 2. archivo en nuestro lenguaje pasado como parametro
+# 1. archivo en nuestro lenguaje pasado como parametro
+# 2. .out compilado despues de YACC y LEX (parser)
 # 3. archivo salida en C tras su transofrmacion
 
-#    1.       2.    3.
-./main.out < $1 > aux.c 
+# 1.      2.        3.
+$1 > ./main.out > aux.c 
 
 # 4. nombre de ejecutable final pasado como parametro
 
